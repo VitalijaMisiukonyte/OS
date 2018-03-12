5703222c470311622a75b106f8b52eb4df5b2fee
Misiukonytė Vitalija <vitalija.misiukonyte@mif.stud.vu.lt>
2018-03-12 17:58:13 +0200

    Started main.

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		RealMachine realMachine = new RealMachine();
+		HashMap<String, Integer> command = new HashMap<String, Integer>();
+		
+	}
+
+}
