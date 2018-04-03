2acdba11148b8cda3d83c2fbef7beeeb5e347e06
ggabriuss <ggabriusee@gmail.com>
2018-04-03 12:34:08 +0300

    dar kai kas

--- OS_C/main.c
+++ OS_C/main.c
@@ -15,9 +15,11 @@ int main(int argc, char *argv[]){
     int end = 0;
 
     while (!end){
+      show_Memory();
+      show_Registers(); 
       printf("Choose menu number:\n");
-      printf("* 1 next command execution: ");
-      short PC = get_pc(); 
+      printf("* 1 execute: ");
+      short PC = get_pc();
       char character_pc[6] = {  (memory[PC/10][PC%10] & 0xFF0000000000) / 0x10000000000,
                                 (memory[PC/10][PC%10] & 0xFF00000000) / 0x100000000,
                                 (memory[PC/10][PC%10] & 0xFF000000) / 0x1000000,
@@ -31,25 +33,19 @@ int main(int argc, char *argv[]){
                                   character_pc[4],
                                   character_pc[5]
           );
-      printf("* 2 Show registers *\n");
-      printf("* 3 Show memory *\n");
-      printf("* 4 Exit *\n");
+      printf("* 2 Exit *\n");
       char in;
       scanf(" %c",&in);
       getchar(); 
       switch (in){
         case '1': end = !nextCommand();
 	               break;
-        case '2': show_Registers();
-	               break;
-        case '3': show_Memory();
-	               break;
-        case '4': end = 1;
+        case '2':  end = 1;
 	               break;
         default: printf("Bad command number\n");
       }
     }
-    printf("Ending Virtual Machine...\n");
+    printf("Exiting Virtual Machine...\n");
   	
   return 0;
 }
